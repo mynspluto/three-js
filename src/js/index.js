01507729221088fed6e8cc153b98b202249d9983
mynspluto <mynspluto@gmail.com>
2024-03-02 00:35:05 +0900

    material 예제

--- src/js/index.js
+++ src/js/index.js
@@ -29,17 +29,48 @@ const light = new THREE.DirectionalLight(0xffffff);
 light.position.set(2, 4, 3);
 scene.add(light);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshStandardMaterial({
   color: 0x2e6ff2,
 });
-const box = new THREE.Mesh(geometry, material);
-scene.add(box);
+
+// 육면체
+const geo1 = new THREE.BoxGeometry(1, 1, 1);
+const obj1 = new THREE.Mesh(geo1, material);
+//scene.add(obj1);
+
+// 원뿔
+const geo2 = new THREE.ConeGeometry(0.5, 1, 32);
+const obj2 = new THREE.Mesh(geo2, material);
+//scene.add(obj2);
+
+// 원기둥
+const geo3 = new THREE.CylinderGeometry(0.5, 0.8, 1, 8);
+const obj3 = new THREE.Mesh(geo3, material);
+//scene.add(obj3);
+
+// 구
+const geo4 = new THREE.SphereGeometry(1);
+const obj4 = new THREE.Mesh(geo4, material);
+//scene.add(obj4);
+
+// 평면
+const geo5 = new THREE.PlaneGeometry(1, 2);
+const obj5 = new THREE.Mesh(geo5, material);
+//scene.add(obj5);
+
+// 원
+const geo6 = new THREE.CircleGeometry(1, 32);
+const obj6 = new THREE.Mesh(geo6, material);
+//scene.add(obj6);
+
+// 튜브
+const geo7 = new THREE.TorusGeometry(1, 0.3);
+const obj7 = new THREE.Mesh(geo7, material);
+scene.add(obj7);
 
 renderer.render(scene, camera);
 
 function animate() {
-  box.rotation.y += 0.01;
   console.log(box.rotation.y);
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
