07478565fa8aac884ee21166ee5202b3fbf8d260
mynspluto <mynspluto@gmail.com>
2024-03-05 18:12:15 +0900

    한라봉 파일 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import printTree from "../js/mesh/tree.js";
+import printTangerine from "../js/mesh/tangerine.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffe287);
@@ -11,20 +12,28 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(5, 5, 5);
+camera.position.set(10, 10, 10);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const light = new THREE.DirectionalLight(0xffffff);
+const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(2, 4, 3);
 scene.add(light);
+light.castShadow = true;
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
 // 나무
 const tree = printTree();
 scene.add(tree);
+
+const tangerine = printTangerine();
+tangerine.position.x = 3;
+scene.add(tangerine);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
